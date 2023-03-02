b387b5760101ad56c065dd4a67fc04df49ab6174
Caio <caiusito@gmail.com>
2023-03-01 21:48:38 -0300

    feat. express.static imagens

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use('/login', routes.loginRouter);
 app.use('/register', routes.userRouter);
 app.use('/products', routes.productRouter);
+app.use('/images', express.static('../../public'));
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
