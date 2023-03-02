30780726e9edd2a8efe49168544202c8e622a698
Caio <caiusito@gmail.com>
2023-03-01 21:57:33 -0300

    fix. Organization of images

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use('/login', routes.loginRouter);
 app.use('/register', routes.userRouter);
 app.use('/products', routes.productRouter);
-app.use('/images', express.static('../../public'));
+app.use('/images', express.static('../../public/images'));
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
