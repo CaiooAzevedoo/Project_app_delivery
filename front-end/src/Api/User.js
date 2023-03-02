0854f3288be4b8cac783fcd530e3c900043ee8d1
DanielDeJesusAraujo <danielaraljo115@gmail.com>
2023-03-01 22:20:25 -0300

    feat: implement cardProducts

--- front-end/src/Api/User.js
+++ front-end/src/Api/User.js
@@ -1,7 +1,8 @@
-import HEADERS_POST from './Utils/User';
+import HEADERS_POST, { HEADERS_GET } from './Utils/User';
 
 const urlLogin = `http://localhost:${process.env.REACT_APP_BACKEND_PORT || '3001'}/login`;
 const urlCreate = `http://localhost:${process.env.REACT_APP_BACKEND_PORT || '3001'}/register`;
+const urlProducts = `http://localhost:${process.env.REACT_APP_BACKEND_PORT || '3001'}/products`;
 
 const getUser = async ({ email, password }) => {
   const payLoad = { email, password };
@@ -32,6 +33,19 @@ export const createUser = async ({ name, email, password }) => {
       },
     );
 
+    return {
+      status: response.status,
+      date: await response.json(),
+    };
+  } catch (error) {
+    const status = 404;
+    return status;
+  }
+};
+
+export const getProducts = async () => {
+  try {
+    const response = await fetch(urlProducts, HEADERS_GET);
     return {
       status: response.status,
       date: await response.json(),
