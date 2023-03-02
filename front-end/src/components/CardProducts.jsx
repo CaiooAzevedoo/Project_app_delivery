2c4aa8cb5883bae94146da27c9cae3b5b389ddb4
DanielDeJesusAraujo <danielaraljo115@gmail.com>
2023-03-02 19:42:00 -0300

    feat: req 15.0

--- front-end/src/components/CardProducts.jsx
+++ front-end/src/components/CardProducts.jsx
@@ -1,14 +1,51 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './styles/CardProducts.css';
 import MainContext from '../context/MainContext';
 
 function CardProducts({ iten }) {
   const { cardState, setCardState } = useContext(MainContext);
+  const [inputValue, setInputValue] = useState(0);
 
-  const handleChange = ({ target: { value, name } }) => {
-    
+  useEffect(() => {
+    const order = cardState.orders.find((element) => element.id === Number(iten.id));
+    if (order) {
+      setInputValue(order.quantity);
+    }
+  }, [cardState.orders]);
+
+  const updateQuantity = (array, id, quant) => {
+    const newList = array.map((element) => {
+      if (element.id === id) {
+        return { ...element, quantity: element.quantity + quant };
+      }
+      return element;
+    });
+    return newList.filter((order) => order.quantity > 0);
   };
+
+  const handleClick = ({ target: { id, name, value } }) => {
+    const decrement = -1;
+    let valueQuantity = name === 'add' ? 1 : decrement;
+    if (name === 'quantity') valueQuantity = value;
+    const alreadyExists = cardState.orders.some((prod) => prod.id === Number(id));
+    if (!alreadyExists) {
+      setCardState((prev) => (
+        {
+          orders: [
+            ...prev.orders,
+            { ...iten, quantity: 1 },
+          ] }));
+    } else {
+      setCardState((prev) => (
+        {
+          orders: updateQuantity(prev.orders, Number(id), valueQuantity),
+        }
+      ));
+    }
+  };
+
+  // const handleChange = () => {};
 
   return (
     <section className="card-products">
@@ -35,6 +72,9 @@ function CardProducts({ iten }) {
         </p>
         <div className="quantity-input-area">
           <button
+            id={ iten.id }
+            name="add"
+            onClick={ handleClick }
             className="btn-add"
             data-testid={ `customer_products__button-card-add-item-${iten.id}` }
             type="button"
@@ -42,15 +82,19 @@ function CardProducts({ iten }) {
             +
           </button>
           <input
-            value={ cardState.quantity }
+            value={ inputValue }
             name="quantity"
+            id={ iten.id }
+            onChange={ handleClick }
             className="input-quantity"
             data-testid={ `customer_products__input-card-quantity-${iten.id}` }
             type="number"
-            onChange={ handleChange }
           />
           <button
             className="btn-rm"
+            name="rm"
+            id={ iten.id }
+            onClick={ handleClick }
             data-testid={ `customer_products__button-card-rm-item-${iten.id}` }
             type="button"
           >
