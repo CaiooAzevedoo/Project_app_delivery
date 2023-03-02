b161effbf8d2c37d4c32f078ebe34ca1591fdd43
DanielDeJesusAraujo <danielaraljo115@gmail.com>
2023-03-02 12:49:05 -0300

    feat: implements style for login products and registration screens

--- front-end/src/components/CardProducts.jsx
+++ front-end/src/components/CardProducts.jsx
@@ -1,44 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './styles/CardProducts.css';
 
 function CardProducts({ iten }) {
   return (
-    <section>
-      <p
-        data-testid={ `customer_products__element-card-price-${iten.id}` }
-      >
-        {
-          iten.price
-        }
-      </p>
-      <img
-        data-testid={ `customer_products__img-card-bg-image-${iten.id}` }
-        src={ iten.urlImage }
-        alt="imagem da bebida"
-      />
-      <p
-        data-testid={ `customer_products__element-card-title-${iten.id}` }
-      >
-        {iten.name}
-      </p>
-      <div>
-        <button
-          data-testid={ `customer_products__button-card-add-item-${iten.id}` }
-          type="button"
+    <section className="card-products">
+      <section>
+        <p
+          data-testid={ `customer_products__element-card-price-${iten.id}` }
         >
-          +
-        </button>
-        <input
-          data-testid={ `customer_products__input-card-quantity-${iten.id}` }
-          type="number"
+          {
+            iten.price
+          }
+        </p>
+        <img
+          className="product-image"
+          data-testid={ `customer_products__img-card-bg-image-${iten.id}` }
+          src={ iten.urlImage }
+          alt="imagem da bebida"
         />
-        <button
-          data-testid={ `customer_products__button-card-rm-item-${iten.id}` }
-          type="button"
+        <p
+          data-testid={ `customer_products__element-card-title-${iten.id}` }
         >
-          -
-        </button>
-      </div>
+          {iten.name}
+        </p>
+        <div className="quantity-input-area">
+          <button
+            className="btn-add"
+            data-testid={ `customer_products__button-card-add-item-${iten.id}` }
+            type="button"
+          >
+            +
+          </button>
+          <input
+            className="input-quantity"
+            data-testid={ `customer_products__input-card-quantity-${iten.id}` }
+            type="number"
+          />
+          <button
+            className="btn-rm"
+            data-testid={ `customer_products__button-card-rm-item-${iten.id}` }
+            type="button"
+          >
+            -
+          </button>
+        </div>
+      </section>
     </section>
   );
 }
