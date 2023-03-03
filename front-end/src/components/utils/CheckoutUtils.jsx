a59e71c9ddde3d9dc9d24ebd833e711dbaf35a01
DanielDeJesusAraujo <danielaraljo115@gmail.com>
2023-03-03 18:17:38 -0300

    feat: req 19 in progress

---
+++ front-end/src/components/utils/CheckoutUtils.jsx
@@ -0,0 +1,11 @@
+import { getLocalStorage } from '../../localstorage';
+
+const removeItenLocalStorage = (key, id) => {
+  const conversionBase = 10;
+  const currentList = getLocalStorage(key);
+  const newList = currentList.filter((iten) => (
+    parseInt(iten.id, conversionBase) === parseInt(id, conversionBase)));
+  return newList;
+};
+
+export default removeItenLocalStorage;
