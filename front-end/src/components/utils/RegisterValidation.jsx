e9512f7f979530fc70da988a374ca17c795cefc0
DanielDeJesusAraujo <danielaraljo115@gmail.com>
2023-03-03 12:10:21 -0300

    feat: req 15 refactor

--- front-end/src/components/utils/RegisterValidation.jsx
+++ front-end/src/components/utils/RegisterValidation.jsx
@@ -3,16 +3,4 @@ const validateName = (name) => {
   return name.length >= minLengthPassword;
 };
 
-export const updateQuantity = (array, id, quant, input = false) => {
-  const newList = array.map((element) => {
-    if (element.id === id) {
-      return {
-        ...element,
-        quantity: input ? quant : element.quantity + quant };
-    }
-    return element;
-  });
-  return newList.filter((order) => order.quantity > 0);
-};
-
 export default validateName;
