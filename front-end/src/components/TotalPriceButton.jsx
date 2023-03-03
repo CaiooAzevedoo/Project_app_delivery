3aae2270e29a8b7f61144c66aa8370a950a73e73
DanielDeJesusAraujo <danielaraljo115@gmail.com>
2023-03-03 01:24:58 -0300

    feat: req 15 and 16 done

--- front-end/src/components/TotalPriceButton.jsx
+++ front-end/src/components/TotalPriceButton.jsx
@@ -1,15 +1,22 @@
+import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import MainContext from '../context/MainContext';
+import { getLocalStorage } from '../localstorage';
+import MainContext from '../context/MainContext';
 
 function TotalPriceButton() {
   const navigate = useNavigate();
-  // const { cardList: [{ x }] } = useContext(MainContext); // pegar os dados do localStorage
+  const { cardState } = useContext(MainContext);
+  const [productsOfLocal, setProductsOfLocal] = useState([]);
 
-  const cardList = [{ price: 2, quantity: 2 }, { price: 4, quantity: 4 }]; // mock
-  const totalByProd = cardList.map((prod) => prod.price * prod.quantity);
+  useEffect(() => {
+    const cardList = getLocalStorage('carrinho');
+    setProductsOfLocal(cardList);
+  }, [cardState]);
+
+  const totalByProd = productsOfLocal.map((prod) => prod.price * prod.quantity);
   const initialValue = 0;
   const total = totalByProd.reduce(
-    (accumulator, currentValue) => accumulator + currentValue,
+    (accumulator, currentValue) => (accumulator + currentValue),
     initialValue,
   );
   console.log(total);
@@ -17,15 +24,22 @@ function TotalPriceButton() {
   return (
     <div>
       <button
-        dataTestId="customer_products_totalprice"
+        type="button"
+        data-testid="customer_products__button-cart"
+        disabled={ productsOfLocal.length < 1 }
+        onClick={ () => navigate('/customer/checkout') }
+      >
+        Ver Carrinho: R$
+      </button>
+      <button
+        data-testid="customer_products__checkout-bottom-value"
+        id="total"
         type="button"
         name="total"
         onClick={ () => navigate('/customer/checkout') }
-        disabled={ cardList.length < 1 }
+        disabled={ productsOfLocal.length < 1 }
       >
-        `Ver Carrinho: R$ $
-        {total}
-        `
+        { productsOfLocal.length > 0 ? String(total.toFixed(2)).replace('.', ',') : 0}
       </button>
     </div>
 
