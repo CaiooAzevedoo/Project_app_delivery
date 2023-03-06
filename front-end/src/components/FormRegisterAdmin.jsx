a532ba40898b94c13324da95cc196c218f40f049
Carina Cunha <carina.cunha13@gmail.com>
2023-03-05 23:00:45 -0300

    feat: add validations to register adm

--- front-end/src/components/FormRegisterAdmin.jsx
+++ front-end/src/components/FormRegisterAdmin.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import validateName from './utils/RegisterValidation';
+import MainContext from '../context/MainContext';
+import { submitIsAllowed } from '../pages/Utils/Login.utils';
 import './styles/FormRegister.css';
 
 function FormRegisterAdmin() {
+  const { register, setRegister } = useContext(MainContext);
+  useEffect(
+    () => {
+      const { email, password, name } = register;
+      if (submitIsAllowed(email, password) && validateName(name)) {
+        setRegister((prev) => ({ ...prev, submitIsDisable: false }));
+      } else setRegister((prev) => ({ ...prev, submitIsDisable: true }));
+    },
+    [register.email, register.password, register.name],
+  );
+
+  const handleChange = ({ target: { value, name } }) => {
+    setRegister((prev) => ({ ...prev, [name]: value }));
+  };
   return (
     <form className="form-container">
       <h1>Cadastro</h1>
@@ -12,6 +29,10 @@ function FormRegisterAdmin() {
           <input
             data-testid="admin_manage__input-name"
             placeholder="nome"
+            type="text"
+            id="name"
+            name="name"
+            onChange={ handleChange }
           />
         </label>
 
@@ -20,6 +41,10 @@ function FormRegisterAdmin() {
           <input
             data-testid="admin_manage__input-email"
             placeholder="email@site.com.br"
+            type="email"
+            id="email"
+            name="email"
+            onChange={ handleChange }
           />
         </label>
 
@@ -28,6 +53,10 @@ function FormRegisterAdmin() {
           <input
             data-testid="admin_manage__input-password"
             placeholder="senha"
+            type="password"
+            id="password"
+            name="password"
+            onChange={ handleChange }
           />
         </label>
 
@@ -37,6 +66,7 @@ function FormRegisterAdmin() {
           <select
             type="select"
             data-testid="admin_manage__select-role"
+            onChange={ handleChange }
           >
             <option value=""> </option>
             <option value="seller">Vendedor</option>
@@ -47,6 +77,7 @@ function FormRegisterAdmin() {
         <button
           type="submit"
           data-testid="admin_manage__button-register"
+          disabled={ register.submitIsDisable }
         >
           CADASTRAR
         </button>
