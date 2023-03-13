1708277f910014e1def92abf111276b60b92ef76
Caio <caiusito@gmail.com>
2023-03-13 18:24:34 -0300

    Fix: Adm Manager Style

--- front-end/src/pages/styles/AdmManageStyles.jsx
+++ front-end/src/pages/styles/AdmManageStyles.jsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 const Background = styled.div`
   display: flex;
-  background-color: rgba(255, 166, 0, 0.537);
   flex-wrap: nowrap;
   flex-direction: column;
 `;
